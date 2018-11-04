a28f4f6354d6084a5e4191da6cedad181ec90dbf
hrbing <hanrb91@163.com>
2018-11-04 14:37:44 +0800

    add pom aar file,according to maven successfully

--- app/src/main/java/com/share/maven/repository/MainActivity.java
+++ app/src/main/java/com/share/maven/repository/MainActivity.java
@@ -3,11 +3,14 @@ package com.share.maven.repository;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.maven.nativerep.NativeRepoTest;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NativeRepoTest.Toast(this);
     }
 }
---
+++ repositoryNative/src/main/java/com/maven/nativerep/NativeRepoTest.java
@@ -0,0 +1,10 @@
+package com.maven.nativerep;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class NativeRepoTest {
+    public static void Toast(Context context) {
+        Toast.makeText(context,"come",Toast.LENGTH_SHORT).show();
+    }
+}
